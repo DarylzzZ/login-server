537f8a9beffdd77d41c510f37815439033dd2da7
Boao Li <liboao1122@hotmail.com>
2020-06-19 09:25:46 +1000

    Add passport dependency
    passport local for { username, password } login

--- src/api/index.js
+++ src/api/index.js
@@ -3,19 +3,10 @@ import userApi from './user'
 
 const router = express.Router()
 
-// router.use(basicAuth({
-//     users: { 'admin': 'admin' },
-//     unauthorizedResponse: (req) => {
-//         return req.auth
-//             ? ('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected')
-//             : 'No credentials provided'
-//     }
-// }))
-
 router.get('/', (req, res) => {
-  res.json({
-    message: 'Hello Api'
-  })
+    res.json({
+        message: 'Hello Api'
+    })
 })
 
 router.use('/users', userApi)
--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import passport from 'passport'
+import passportLocal from 'passport-local'
 
 import config from '../config'
 import db from '../models'
@@ -8,88 +10,94 @@ const router = express.Router()
 
 const { User } = db.sequelize.models
 
+router.use(passport.initialize())
 /**
  * api get all users
  */
 router.get('/', (req, res) => {
-  User.findAll({ attributes: { exclude: ['password'] } })
-    .then(users =>
-      res.status(200).json({
-        success: true,
-        users
-      })
-    )
-    .catch(err =>
-      res.status(500).json({
-        success: false,
-        message: err.message
-      })
-    )
+    User.findAll({ attributes: { exclude: ['password'] } })
+        .then((users) =>
+            res.status(200).json({
+                success: true,
+                users
+            })
+        )
+        .catch((err) =>
+            res.status(500).json({
+                success: false,
+                message: err.message
+            })
+        )
 })
 
 /**
  * api login
  *
  */
-router.post('/login', (req, res) => {
-  const { username, password } = req.body
+passport.use(
+    new passportLocal.Strategy(function (username, password, done) {
+        User.findOne({ where: { username } })
+            .then((user) => {
+                if (user) {
+                    return user.validatePassword(password)
+                } else {
+                    throw new Error('User not found.')
+                }
+            })
+            .then((pass) => {
+                if (pass)
+                    done(null, {
+                        success: true,
+                        user: { username: username },
+                        token: jwt.sign({ username }, config.jwtsecret)
+                    })
+                else throw new Error('incorrect password')
+            })
+            .catch((err) => {
+                done(null, { success: false, message: err.message })
+            })
+    })
+)
 
-  User.findOne({ where: { username } })
-    .then(user => {
-      if (user) {
-        return user.validatePassword(password)
-      } else {
-        throw new Error('User not found.')
-      }
-    })
-    .then(pass =>
-      pass
-        ? res.status(200).json({
-            success: true,
-            user: { username: username },
-            token: jwt.sign({ username }, config.jwtsecret)
-          })
-        : res.status(200).json({
-            success: false,
-            message: 'incorrect password'
-          })
-    )
-    .catch(err => {
-      res.status(200).json({
-        success: false,
-        message: err.message
-      })
-    })
-})
+router.post(
+    '/login',
+    passport.authenticate('local', { session: false }),
+    (req, res) => {
+        res.status(200).json({ ...req.user })
+    }
+)
 
 /**
  * api signup
  *
  */
 router.post('/', (req, res) => {
-  const { username, password } = req.body
+    const { username, password } = req.body
 
-  if (username && password) {
-    User.create({ username, password })
-      .then(user =>
+    if (username && password) {
+        User.create({ username, password })
+            .then((user) =>
+                res.status(200).json({
+                    success: true,
+                    user: { username: user.username },
+                    token: jwt.sign(
+                        { username: user.username },
+                        config.jwtsecret
+                    )
+                })
+            )
+            .catch((err) =>
+                res.status(200).json({
+                    success: false,
+                    message: err.message
+                })
+            )
+    } else {
         res.status(200).json({
-          success: true,
-          user: { username: user.username },
-          token: jwt.sign({ username: user.username }, config.jwtsecret)
+            success: false,
+            message: 'Invalid request'
         })
-      )
-      .catch(err =>
-        res.status(200).json({
-          success: false,
-          message: err.message
-        })
-      )
-  } else {
-    res.status(200).json({
-      success: false,
-      message: 'Invalid request'
-    })
-  }
+    }
 })
 
 export default router
